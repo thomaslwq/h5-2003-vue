0f00794fd33d42c1823954f02c7780c83b0dcb63
unknown <lijiahao_work@163.com>
2020-08-03 19:44:27 +0800

    布局

--- lijiahao/backend/src/views/Home/Home.vue
+++ lijiahao/backend/src/views/Home/Home.vue
@@ -1,19 +1,90 @@
 <template>
+    <el-container>
+        <el-header>Header</el-header>
+        <el-container class="app-el-container">
+            <el-aside width="200px">
+                <el-menu
+                        default-active="2"
+                        class="el-menu-vertical-demo"
+                        @open="handleOpen"
+                        @close="handleClose"
+                        background-color="#545c64"
+                        text-color="#fff"
+                       >
+                    <el-menu-item index="1">
+                        <i class="el-icon-s-tools"></i>
+                        <span slot="title">系统首页</span>
+                    </el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title">
+                            <i class="el-icon-goods"></i>
+                            <span>商品管理</span>
+                        </template>
+                        <el-menu-item-group>
 
-       <el-container class="app-el-container">
-           <el-aside width="200px">Aside</el-aside>
-           <el-container>
-               <el-header>Header</el-header>
-               <el-main>Main</el-main>
-           </el-container>
-       </el-container>
+                            <el-menu-item index="2-1">选项1</el-menu-item>
+                            <el-menu-item index="2-2">选项2</el-menu-item>
 
+                        </el-menu-item-group>
+
+                    </el-submenu>
+                    <el-submenu index="3">
+                        <template slot="title">
+                            <i class="el-icon-sell"></i>
+                            <span>交易管理</span>
+                        </template>
+                        <el-menu-item-group>
+
+                            <el-menu-item index="3-1">选项1</el-menu-item>
+                            <el-menu-item index="3-2">选项2</el-menu-item>
+
+                        </el-menu-item-group>
+
+                    </el-submenu>
+                    <el-submenu index="4">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>活动管理</span>
+                        </template>
+                        <el-menu-item-group>
+
+                            <el-menu-item index="4-1">选项1</el-menu-item>
+                            <el-menu-item index="4-2">选项2</el-menu-item>
+
+                        </el-menu-item-group>
+
+                    </el-submenu>
+                    <el-submenu index="5">
+                        <template slot="title">
+                            <i class="el-icon-message-solid"></i>
+                            <span>消息管理</span>
+                        </template>
+                        <el-menu-item-group>
+
+                            <el-menu-item index="5-1">选项1</el-menu-item>
+                            <el-menu-item index="5-2">选项2</el-menu-item>
+
+                        </el-menu-item-group>
+
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <el-main>Main的撒大大大法国</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-
+    methods: {
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    }
 };
 
 </script>
@@ -23,14 +94,14 @@ export default {
         height: 100vh;
     }
     .el-header, .el-footer {
-        background-color: #B3C0D1;
+        background-color: rgb(67,142,185);
         color: #333;
-        text-align: center;
+        /*text-align: center;*/
         line-height: 60px;
     }
 
     .el-aside {
-        background-color: #D3DCE6;
+        background-color: rgb(84,92,100);
         color: #333;
         text-align: center;
         line-height: 200px;
@@ -39,8 +110,6 @@ export default {
     .el-main {
         background-color: #E9EEF3;
         color: #333;
-        text-align: center;
-        line-height: 160px;
     }
 
     body > .el-container {
