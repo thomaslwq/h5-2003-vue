c13e818e621fc64b044e25ce8dd386753e6c1959
vk0413hwq <963561754@qq.com>
2020-08-03 19:55:53 +0800

    完成了登录和退出功能

--- heweiqi/src/main.js
+++ heweiqi/src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+// 引入axios
+import axios from "./common/myaxios"
+// 挂载axiosS
+Vue.prototype.$axios = axios
+
+// 导入全局样式表
+import './assets/css/global.css'
+
+// Vue.use(HighchartsVue)
+// 按需引入组件
+
+
+
+
 
 Vue.config.productionTip = false
 
--- heweiqi/src/router/index.js
+++ heweiqi/src/router/index.js
@@ -1,29 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import login from '../components/login/login.vue'
-
+import Home from './../components/Home.vue'
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/login',
-    name: 'Login',
-    component:login
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/',
-    name: 'Login',
-    redirect:'login'
-  }
+const routes = [
+  { path: '/', name: 'login', redirect: '/login' },
+  { path: '/login', name: 'login', component: login },
+  { path: '/home', name: 'home', component: Home },
+
 ]
 
 const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next();
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
