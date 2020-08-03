b4ab47845c92c15f3806c13ad5b1445005bd2a0a
unknown <lijiahao_work@163.com>
2020-08-03 17:20:40 +0800

    布局

--- lijiahao/backend/src/views/Home/Home.vue
+++ lijiahao/backend/src/views/Home/Home.vue
@@ -1,32 +1,13 @@
 <template>
-   <div>
-       <el-row>
-           <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
-       </el-row>
-       <el-row>
-           <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
-           <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
-       </el-row>
-       <el-row>
-           <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-           <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
-           <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-       </el-row>
-       <el-row>
-           <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-           <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-           <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-           <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-       </el-row>
-       <el-row>
-           <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-           <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-           <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-           <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-           <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-           <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-       </el-row>
-   </div>
+
+       <el-container class="app-el-container">
+           <el-aside width="200px">Aside</el-aside>
+           <el-container>
+               <el-header>Header</el-header>
+               <el-main>Main</el-main>
+           </el-container>
+       </el-container>
+
 </template>
 
 <script>
@@ -37,30 +18,41 @@ export default {
 
 </script>
 <style lang="less">
-    .el-row {
-        margin-bottom: 20px;
-        &:last-child {
-            margin-bottom: 0;
-        }
+
+    .app-el-container{
+        height: 100vh;
     }
-    .el-col {
-        border-radius: 4px;
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
     }
-    .bg-purple-dark {
-        background: #99a9bf;
+
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
     }
-    .bg-purple {
-        background: #d3dce6;
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        line-height: 160px;
     }
-    .bg-purple-light {
-        background: #e5e9f2;
+
+    body > .el-container {
+        margin-bottom: 40px;
     }
-    .grid-content {
-        border-radius: 4px;
-        min-height: 36px;
+
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+        line-height: 260px;
     }
-    .row-bg {
-        padding: 10px 0;
-        background-color: #f9fafc;
+
+    .el-container:nth-child(7) .el-aside {
+        line-height: 320px;
     }
 </style>
