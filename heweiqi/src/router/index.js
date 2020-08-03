41a71fe1ce72b68c107cd9bbc373652e9e76a76f
vk0413hwq <963561754@qq.com>
2020-08-03 12:46:41 +0800

    init
    init

--- heweiqi/src/router/index.js
+++ heweiqi/src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import login from '../components/login/login.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component:login
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/',
+    name: 'Login',
+    redirect:'login'
   }
 ]
 
--- heweiqi/vue.config.js
+++ heweiqi/vue.config.js
@@ -1,3 +1,5 @@
-export default= {
-    
+module.exports = {
+    lintOnSave:false,
+    publicPath:"./",
+    productionSourceMap:false, 
 }
