fa15e7731f14dbb2610de2ce730884ab2df2c160
Zeratul_X <382018999@qq.com>
2020-08-04 16:58:35 +0800

    axios配置 3.0&后台配置&apiServer加入项目 by:Zeratul 2020年8月4日16:58:33

--- zhangminhui/vue-admin/src/router/index.js
+++ zhangminhui/vue-admin/src/router/index.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 Vue.use(Router);
-
+// 解决ElementUI导航栏中的vue-router在3.0版本以上重复点菜单报错问题
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 export default new Router({
     mode:'history',
     routes: [{
@@ -24,56 +28,56 @@ export default new Router({
                 },
                 {
                     path: '/userManage',
-                    component: () => import('../components/page/BaseTable.vue'),
+                    component: () => import('../components/page/UserManage.vue'),
                     meta: {
                         title: '用户管理'
                     }
                 },
                 {
                     path: '/productManage',
-                    component: () => import('../components/page/Tabs.vue'),
+                    component: () => import('../components/page/ProductManage.vue'),
                     meta: {
                         title: '商品管理'
                     }
                 },
                 {
                     path: '/colorManage',
-                    component: () => import( '../components/page/BaseForm.vue'),
+                    component: () => import( '../components/page/ColorManage.vue'),
                     meta: {
                         title: '颜色管理'
                     }
                 },
                 {
                     path: '/matetialManage',
-                    component: () => import( '../components/page/VueEditor.vue'),
+                    component: () => import( '../components/page/MatetialManage.vue'),
                     meta: {
                         title: '材料管理'
                     }
                 },
                 {
                     path: '/seriesManage',
-                    component: () => import( '../components/page/Markdown.vue'),
+                    component: () => import( '../components/page/SeriesManage.vue'),
                     meta: {
                         title: '系列管理'
                     }
                 },
                 {
                     path: '/sortManage',
-                    component: () => import( '../components/page/Upload.vue'),
+                    component: () => import( '../components/page/SortManage.vue'),
                     meta: {
                         title: '分类管理'
                     }
                 },
                 {
                     path: '/specificationManage',
-                    component: () => import('../components/page/BaseCharts.vue'),
+                    component: () => import('../components/page/SpecificationManage.vue'),
                     meta: {
                         title: '规则管理'
                     }
                 },
                 {
                     path: '/commetManage',
-                    component: () => import( '../components/page/BaseCharts.vue'),
+                    component: () => import( '../components/page/CommetManage.vue'),
                     meta: {
                         title: '评论管理'
                     }
