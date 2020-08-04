67308c4be37a04823bc76aee16c8265ed08c1d29
wangwenmei <3171376445@qq.com>
2020-08-04 09:14:29 +0800

    代理商列表和代理商添加静态组件

--- luoshihao/my-app/src/components/merchant.vue
+++ luoshihao/my-app/src/components/merchant.vue
@@ -1,3 +1,154 @@
 <template>
-    <div>代理商管理</div>
+<div id="list">
+<div class="list-nav">
+  <router-link to="/">代理商管理</router-link > &gt; <router-link to="">代理商列表</router-link>
+</div>
+<div class="list-search">
+  <section class="search-input">
+    <input type="text" placeholder="请输入代理商名称"/>
+  </section>
+  <section class="search-btn">检索</section>
+</div>
+  <el-table
+    :data="tableData"
+    border>
+    <el-table-column
+      fixed
+      prop="agent"
+      label="代理商名称"
+      width="100">
+    </el-table-column>
+    <el-table-column
+      prop="code"
+      label="代理商编码"
+      width="200">
+    </el-table-column>
+    <el-table-column
+      prop="region"
+      label="所属区域"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="leader"
+      label="负责人"
+      width="75">
+    </el-table-column>
+    <el-table-column
+      prop="login"
+      label="登录名"
+      width="75">
+    </el-table-column>
+    <el-table-column
+      prop="contact"
+      label="联系方式"
+      width="114">
+    </el-table-column>
+    <el-table-column
+      prop="fax"
+      label="传真"
+      width="96">
+    </el-table-column>
+    <el-table-column
+      prop="mail"
+      label="邮箱"
+      width="161">
+    </el-table-column>
+    <el-table-column
+      label="操作"
+      width="120">
+      <template slot-scope="scope">
+        <el-button class="check" @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+        <el-button class="editor" type="text" size="small">编辑</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</div>
 </template>
+
+<script>
+ export default {
+    methods: {
+      handleClick(row) {
+        console.log(row);
+      }
+    },
+
+    data() {
+      return {
+        tableData: [{
+          agent: '天天向上',
+          code: 'DLS201802281450280741',
+          region: '无锡市',
+          leader: '马克思',
+          login: '马克思',
+          contact: 18600001111,
+          fax:'028-6666666',
+          mail:'123456789@qq.com'
+        },
+        {
+          agent: '天天向上',
+          code: 'DLS201802281450280741',
+          region: '无锡市',
+          leader: '马克思',
+          login: '马克思',
+          contact: 18600001111,
+          fax:'028-6666666',
+          mail:'123456789@qq.com'
+        }]
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+.el-main{
+    line-height:0;
+    text-align: left;
+}
+.list-nav{
+  height: 50px;
+  border-bottom: 1px solid #909399;
+  padding-left:10px;
+  margin-top: 20px;
+  a{
+    text-decoration: none;
+    color:#333;
+    font-size: 18px;
+    margin-right:8px
+  }
+
+}
+.list-search{
+  display:flex;
+  padding:10px 0 15px 0;
+  .search-input{
+    width:190px;
+    height:38px;
+    border:1px solid #909399;
+    border-radius:2px;
+    margin-left:10px;
+    box-sizing: border-box;
+    input{
+      border: none;
+      padding-left: 10px;
+      width:100%;
+      height:100%;
+    }
+  }
+  .search-btn{
+    width:64px;
+    height:38px;
+    background: #009688;
+    color:#fff;
+    line-height: 38px;
+    text-align: center;
+    margin-left:20px;
+  }
+  button .el-button .el-button--text .el-button--small{
+    width: 100px;
+    height:50px;
+    background-color: #009688 !important;
+  }
+}
+
+</style>
--- luoshihao/my-app/src/components/merchantAdd.vue
+++ luoshihao/my-app/src/components/merchantAdd.vue
@@ -1,3 +1,82 @@
 <template>
-    <div>代理商添加</div>
+  <div id="addform">
+    <div class="addform-nav">
+      <router-link to="/">代理商管理</router-link > &gt; <router-link to="">添加代理商</router-link>
+    </div>
+    <div class="addform-content">
+        <el-form ref="form" :model="form" label-width="100px">
+    <el-form-item label="代理商名称">
+      <el-input v-model="form.agent"></el-input>
+    </el-form-item>
+    <el-form-item label="负责人">
+      <el-input v-model="form.leader"></el-input>
+    </el-form-item>
+    <el-form-item label="登录名">
+      <el-input v-model="form.login"></el-input>
+    </el-form-item>
+    <el-form-item label="登录密码">
+      <el-input v-model="form.password1"></el-input>
+    </el-form-item>
+    <el-form-item label="确认密码">
+      <el-input v-model="form.password2"></el-input>
+    </el-form-item>
+    <el-form-item label="电话">
+      <el-input v-model="form.contact"></el-input>
+    </el-form-item>
+    <el-form-item label="身份证号">
+      <el-input v-model="form.idcard"></el-input>
+    </el-form-item>
+    <el-form-item label="传真">
+      <el-input v-model="form.fax"></el-input>
+    </el-form-item>
+    <el-form-item label="邮箱">
+      <el-input v-model="form.mail"></el-input>
+    </el-form-item>
+    <el-form-item label="备注">
+      <el-input type="textarea" v-model="form.remarks"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
+    </div>
+  </div>
 </template>
+
+<script>
+ export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+.addform-nav{
+  height: 50px;
+  border-bottom: 1px solid #909399;
+  padding-left:10px;
+  margin-top: 20px;
+  a{
+    text-decoration: none;
+    color:#333;
+    font-size: 18px;
+    margin-right:8px
+  }
+}
+.addform-content{
+  margin-top:10px;
+}
+
+</style>
