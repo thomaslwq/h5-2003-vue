72114141218d2f9a1312c3047df84927c2ab9cea
wangwenmei <3171376445@qq.com>
2020-08-05 21:11:26 +0800

    完善

--- luoshihao/my-app/src/main.js
+++ luoshihao/my-app/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import axios from './common/myaxios'
+import axios from 'axios'
 
 
 Vue.prototype.$axios = axios;
