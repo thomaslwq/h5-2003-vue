5188882aa5ee1e0dc056342c28a57ab5af109223
liyanwei741 <13450166620@163.com>
2020-08-06 19:20:01 +0800

    更新主分支

--- luoshihao/my-app/src/components/merchantAdd.vue
+++ luoshihao/my-app/src/components/merchantAdd.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="addform">
-    <!-- <div class="addform-nav">
-      <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item :to="{ path: '/' }">代理商管理</el-breadcrumb-item>
-        <el-breadcrumb-item>添加代理商</el-breadcrumb-item>
-      </el-breadcrumb>
-    </div> -->
     <div class="addform-content">
       <el-form
         ref="form"
